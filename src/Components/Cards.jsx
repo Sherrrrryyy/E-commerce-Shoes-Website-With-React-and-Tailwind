49c2c84cc4fbfbab1c17506f5187b07e71be3e8f
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-01-04 02:27:22 +0500

    add FontAwesome icons to Navbar and update Cards component layout

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -6,8 +6,8 @@ import Image2 from '../assets/Images/Card2.jpg'
 const Cards = () => {
     return (
         <>
-            <div className="container px-5 py-24 mx-auto flex gap-5">
-                <div className="transform transition-transform duration-500 hover:scale-105 lg:w-1/3 w-full mb-10 lg:mb-0 overflow-hidden shadow-inner">
+            <div className="container flex-col px-5 py-24 mx-auto md:flex-row flex-1 flex gap-5">
+                <div className=" transform transition-transform duration-500 hover:scale-105 lg:w-1/3  w-full mb-10 lg:mb-0 overflow-hidden shadow-inner">
                     <img
                         alt="feature"
                         className="object-cover object-center h-full w-full"
@@ -24,14 +24,14 @@ const Cards = () => {
                 </div>
                 <div className="transform transition-transform duration-500 hover:scale-105 lg:w-1/3 w-full mb-10 lg:mb-0 overflow-hidden shadow-inner">
                     <video
-                    preload='auto'
-                    autoPlay
-                    loop
-                    playsInline
-                    webkit-playsinline
-                    x5-playsinline
-                    src='//cdn.allbirds.com/video/upload/f_auto,q_auto/cms/6JzgSRWxQcOKC2NjmFgj7k/a8ea972dad08fcb00d8a58804ea1ac55/24Q3_MizzleRelaunch_WorldOfDry_WoolRunnerMizzle_Homepage_Tri-Panel_Module_Desktop_Mobile_1200x1500.mp4'></video>
-      <div className="absolute bottom-0 inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-500">
+                        preload='auto'
+                        autoPlay
+                        loop
+                        playsInline
+                        webkit-playsinline
+                        x5-playsinline
+                        src='//cdn.allbirds.com/video/upload/f_auto,q_auto/cms/6JzgSRWxQcOKC2NjmFgj7k/a8ea972dad08fcb00d8a58804ea1ac55/24Q3_MizzleRelaunch_WorldOfDry_WoolRunnerMizzle_Homepage_Tri-Panel_Module_Desktop_Mobile_1200x1500.mp4'></video>
+                    <div className="absolute bottom-0 inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-500">
                         <button className="inline-flex text-black shadow-lg bg-white border-0 py-2 px-6 focus:outline-none hover:bg-black hover:text-white rounded text-lg transition-colors duration-500 mx-2">
                             SHOP MEN
                         </button>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCartShopping, faMagnifyingGlass, faUser} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
-    <header className="text-black-600 body-font shadow-lg sticky top-0 bg-white z-10">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+    <header className="text-black-600 shadow-lg -mt-5 sticky top-0 bg-white z-10">
+  <div className="container mx-auto  flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <nav className="flex lg:w-2/5 md:mb-0 sm:mb-4 flex-wrap items-center text-base md:ml-auto">
       <a className="mr-5 hover:underline cursor-pointer">MEN</a>
       <a className="mr-5 hover:underline cursor-pointer">WOMEN</a>
       <a className="mr-5 text-red-600 font-bold hover:underline cursor-pointer">SALE</a>
@@ -15,6 +17,10 @@ const Navbar = () => {
     </a>
     <div className="lg:w-2/5 gap-10 inline-flex lg:justify-end ml-5 lg:ml-0">
     <a className="mr-5 hover:underline cursor-pointer">RERUN</a>
+    <FontAwesomeIcon className='cursor-pointer text-2xl' icon={faUser} />
+    <FontAwesomeIcon className='cursor-pointer text-2xl' icon={faMagnifyingGlass} />
+    <FontAwesomeIcon className='cursor-pointer text-2xl' icon={faCartShopping} />
+
      
     </div>
   </div>
